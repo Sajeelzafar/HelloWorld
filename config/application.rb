396b525434cf1cf30fe1b09905fd914e4bad5c60
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-03-26 02:12:00 +0500

    Installing gems and dependencies and displaying message on front end

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Test2
+module Helloworldoneapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root "root#index"
 
+  namespace :api do
+    get '/messages', to: 'messages#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'root#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
